c66bf5d4f0e87cfc6414dfdd942147f4d86ee62b
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2026-02-05 12:57:26 +0100

    fix: don't assign IDs to warriors stored in training documents' "warriors" array

--- src/models/training.model.js
+++ src/models/training.model.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
-const warriorSchema = new mongoose.Schema({
-  name: String,
-  weaponName: String,
-  durability: Number,
-  gold: Number,
-  state: { type: String, enum: ["training", "finished"] },
-});
+const warriorSchema = new mongoose.Schema(
+  {
+    name: String,
+    weaponName: String,
+    durability: Number,
+    gold: Number,
+    state: { type: String, enum: ["training", "finished"] },
+  },
+  { _id: false },
+);
 
 const trainingSchema = new mongoose.Schema({
   epicDate: String,
