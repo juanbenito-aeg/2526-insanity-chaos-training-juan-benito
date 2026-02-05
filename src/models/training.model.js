20d97850790db4ad214a08ad7f16fa377d32aa68
juanbenito-aeg <juan.benito@ikasle.aeg.eus>
2026-02-05 10:57:40 +0100

    feat: create 'Training' model

---
+++ src/models/training.model.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const warriorSchema = new mongoose.Schema({
+  name: String,
+  weaponName: String,
+  durability: Number,
+  gold: Number,
+  state: { type: String, enum: ["training", "finished"] },
+});
+
+const trainingSchema = new mongoose.Schema({
+  epicDate: String,
+  warriors: [warriorSchema],
+});
+
+const Training = mongoose.model("Training", trainingSchema);
+
+module.exports = Training;
